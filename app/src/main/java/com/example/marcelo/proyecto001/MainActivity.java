e052efc9b6087195a64539b4b0b1f6482347cb16
Marcelo F <marcelo@lomasoft.com.ar>
2017-07-11 13:28:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/marcelo/proyecto001/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcelo.proyecto001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
